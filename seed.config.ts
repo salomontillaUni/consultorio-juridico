4b40d6c7d12068d77bdcabed7c93c148ebe4748f
salomon montilla <salomon.montilla.l@uniautonoma.edu.co>
2025-10-22 00:31:04 -0500

    feat: update seed configuration and implement user seeding with Supabase

--- seed.config.ts
+++ seed.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
     await client.connect();
     return new SeedPg(client);
   },
+  select: [
+    // We don't alter any extensions tables that might be owned by extensions
+    "!*",
+    // We want to alter all the tables under public schema
+    "public*",
+    // We also want to alter some of the tables under the auth schema
+    "auth.users",
+    "auth.identities",
+    "auth.sessions",
+  ]
 });
--- seed.ts
+++ seed.ts
@@ -4,17 +4,39 @@
  * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
  */
 import { createSeedClient } from "@snaplet/seed";
+import { copycat } from '@snaplet/copycat';
+import { supabase } from "./src/utils/supabase";
 
 const main = async () => {
-  const seed = await createSeedClient();
-
+  const seed = await createSeedClient({
+    dryRun: true
+  });
   // Truncate all tables in the database
   await seed.$resetDatabase();
 
+  const PASSWORD = "testuser";
+  for (let i = 0; i < 5; i += 1) {
+    const email = copycat.email(i);
+    const nombre_completo: string = copycat.fullName(i);
+    const cedula: string = copycat.phoneNumber.toString();
+    const telefono: string = copycat.phoneNumber.toString();
+    await supabase.auth.signUp({
+        email,
+        password: PASSWORD,
+        options: {
+        data: {
+          nombre_completo,
+          cedula,
+          telefono
+        }
+      }
+    });
+  }
 
   console.log("Database seeded successfully!");
-
   process.exit();
 };
-
 main();
+
+
+
