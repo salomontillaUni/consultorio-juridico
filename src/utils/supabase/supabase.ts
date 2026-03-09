82d03e9f3a7ef141b1ce9bb108f95811035a7f1c
salomontillaUni <salomon.montilla.l@uniautonoma.edu.co>
2026-03-09 12:54:52 -0500

    refactor: use .env variables in supabases client

--- src/utils/supabase/supabase.ts
+++ src/utils/supabase/supabase.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = 'http://127.0.0.1:54321';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
